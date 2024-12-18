b0c596c1d4969d85ae44e1a6b53240d2010c7bf7
Atharva Bakre <atharva.bakre05@gmail.com>
2024-12-18 23:43:44 +0530

    Learnt about Console.log,Template literals, Operators in JS, Conditional statements, Logical operators, truthy&falsy, Switch statement, Alert & prompt

---
+++ Javascript/JS Part 2/Practice Questions/Q3.js
@@ -0,0 +1,4 @@
+let firstName = prompt("enter first name");
+let lastName = prompt("enter last name");
+let msg = "Welcome" + " " + firstName + " " + lastName + "!";
+alert(msg);
---
+++ Javascript/JS Part 2/app.js
@@ -0,0 +1,53 @@
+// console.log("Hello World!");
+// console.log("My Dreams");
+//     let a = 10;
+//     let b = 5;
+//     console.log("sum is:", a + b);
+
+// let pencilPrice = 10;
+// let eraserPrice = 5;
+// let output = "The total price is : " + (pencilPrice + eraserPrice) + " Rupees. ";
+// let output = `The total price is ${pencilPrice + eraserPrice} Rupees. `;
+// console.log(`The total price is ${pencilPrice + eraserPrice} Rupees. `);
+
+
+
+// Arithmatic Operators:
+// let a = 10;
+// let b = 5;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+ 
+//Unary Operators;//
+// console.log(a++);
+// console.log(a);
+// console.log(++a);
+
+//Assignment Operators //
+// a *= b;
+// console.log(b);
+
+
+// If Conditional Statement:
+// console.log("This is my statement before if");
+// age = 30
+// if (age>=18) {
+//     console.log("You are eligible for voting");
+//     console.log(age*5);
+// }
+
+// console.log("This is my statement after if");
+// if(age>25) {
+//     console.log("You should be matured!");
+// }
+
+let firstName = "Atharva";
+if(firstName == "Atharva") {
+    console.log(`Welcome ${firstName}`);
+}
+
+
