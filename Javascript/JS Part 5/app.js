5e2a50f2a954c7464b7e27724fa5e0f0804395d4
Atharva Bakre <atharva.bakre05@gmail.com>
2024-12-30 00:33:53 +0530

    Learnt about Object Literals and its various props

---
+++ Javascript/JS Part 5/app.js
@@ -0,0 +1,83 @@
+// const student = {
+//     class: "XII",
+//     marks: "95",
+//     age: "17"
+// };
+
+// const item = {
+//     price: 100,
+//     discount: 50,
+//     color: ["red", "pink"]
+// };
+
+//Twitter Post
+
+// const post=  {
+//     username: "@atharvabakre05",
+//     content: "myFirstpost",
+//     likes: 250,
+//     reposts: 5,
+//     tags: ["@github", "@rasikabakre"]
+// };
+
+// //To Get Values use "object.key"
+
+//Add/Update Value in Object literals
+// const student = {
+//     name: "Atharva", 
+//     age: 19, 
+//     marks: 100,
+//     city: "Nagpur"
+// };
+
+//Objects of objects
+// const studentInfo = {
+//     atharva: {
+//         grade: "A+",
+//         hobby: "Cricket",
+//         talent: "public speaker"
+//     },
+
+//     rasika: {
+//         grade: "Outstanding",
+//         hobby: "Crochet",
+//         talent: "Irritating"
+//     },
+
+//     aai: {
+//         grade: "A++++++++++++",
+//         hobby: "Cooking",
+//         talent: "Convincer"
+//     },
+
+//     baba: {
+//         grade: "SUPERRRRRRRRRRRRRR",
+//         hobby: "Reading newspaper",
+//         talent: "All rounder"
+//     }
+
+// }
+
+//Arrays of objects
+
+const studentInfo = [
+    {
+        name: "Atharva",
+        grade: "A++",
+        city: "Banglore"
+    },
+
+    {
+        name: "Rasika",
+        grade: "O",
+        city: "Pune"
+    },
+
+    {
+        name: "Mummaa and PAPA",
+        grade: "The bestttt",
+        city: "Nagpur"
+    }
+];
+
+
