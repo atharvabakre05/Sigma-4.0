8acb2e5d52d7de274a4be57d36f06e14139ce03b
Atharva Bakre <atharva.bakre05@gmail.com>
2024-12-30 16:07:56 +0530

    Built a guessing game using JS and practiced some questions based on object literals

---
+++ Javascript/Assignment Questions/Assign 4.js
@@ -0,0 +1,19 @@
+//Q1
+let dice = Math.floor(Math.random()*6) + 1;
+console.log(dice);
+
+//Q2
+const car = {
+    name: "Mclaren",
+    model: "765LT" ,
+    color: "green"
+};
+console.log(car.name);
+
+
+//Q3
+const person = {
+    name: "Atharva",
+    age: "19",
+    city: "Nagpur"
+};
--- README.md
+++
@@ -1,2 +0,0 @@
-# Sigma-4.0
-It's my full stack development practice codes through which I am thriving to become a tech-enthusiast. This comprises of my daily contributions to my learnings and captures my consistency throughout the course!
