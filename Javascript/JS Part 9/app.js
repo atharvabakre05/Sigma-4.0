7260b4b9e0626739fcd5f5583d8a46cf55f0df78
Atharva Bakre <atharva.bakre05@gmail.com>
2025-01-14 22:42:28 +0530

    Learnt aout how to manipulate attributes

---
+++ Javascript/JS Part 9/app.js
@@ -0,0 +1,11 @@
+// let smallImages = document.getElementsByClassName("oldImg");
+// for (i = 0; i < smallImages.length; i++) {
+//     smallImages[i].src = "assets/spiderman_img.png";
+//     console.log(`value of image no. ${i} is changed`);
+// }
+
+//Query Selector
+// console.dir(document.querySelector("p"));
+// console.dir(document.querySelector(".oldImg"));
+// console.dir(document.querySelectorAll("div a")); //This selects all the tags that are there in the html file
+
