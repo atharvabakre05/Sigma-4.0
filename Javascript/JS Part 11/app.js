eaa2913e625836704271788068a73cb34747c5f3
Atharva Bakre <atharva.bakre05@gmail.com>
2025-01-30 23:40:20 +0530

    Learnt about asynchronous function, await keyword, API-intro and JSON files

--- Javascript/JS Part 11/app.js
+++ Javascript/JS Part 11/app.js
@@ -29,19 +29,38 @@
 
 // three();
 
-//
+
 
 //Callback Hell
-// h1 = document.querySelector('h1');
-// function changeColor(color, delay, nextColorChange) {
-//     setTimeout(() => {
-//         h1.style.color = color;
-//         if(nextColorChange) nextColorChange();
-//     },delay);
-// }
+h1 = document.querySelector('h1');
+function changeColor(color, delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            h1.style.color = color;
+            resolve("color changed!");
+        },delay);
+
+    })
+}
+
+changeColor("red", 1000) 
+.then(() => {
+    console.log("red color was completed");
+    return changeColor("orange", 1000);
+})
+
+.then(() => {
+    console.log("orange color was changed successfully");
+    return changeColor("green", 1000);
+})
+
+.then(() => {
+    console.log("green color was completed");
+    return changeColor("blue", 1000);
+})
 
 // changeColor('red' ,1000,() => {
-//     changeColor('orange', 1000,() => {
+//     changeColor('orange', 1000)() => {
 //         changeColor('green', 1000);
 //     });
 
@@ -88,39 +107,39 @@
 
 
 //Reject and Resolve
-function savetoDb(data) {
-    return new Promise((resolve, reject) => {
-        let internetSpeed = Math.floor(Math.random()* 10) + 1;
-        if(internetSpeed>4) {
-            resolve("success: data was resolved");
-        }else{
-            reject("failure: weak connection");
-        }
-    });
-}
-savetoDb("Atharva") 
-    .then( (result) => {
-        console.log("Data1 was saved.");
-        console.log("result of promise: ", result);
-        return savetoDb("Hello world");
-    })
+// function savetoDb(data) {
+//     return new Promise((resolve, reject) => {
+//         let internetSpeed = Math.floor(Math.random()* 10) + 1;
+//         if(internetSpeed>4) {
+//             resolve("success: data was resolved");
+//         }else{
+//             reject("failure: weak connection");
+//         }
+//     });
+// }
+// savetoDb("Atharva") 
+//     .then( (result) => {
+//         console.log("Data1 was saved.");
+//         console.log("result of promise: ", result);
+//         return savetoDb("Hello world");
+//     })
 
-    .then((result) => {
-        console.log("Data2 was saved");
-        console.log("result of promise: ", result);
-        return savetoDb("Rasika");
-    })
+//     .then((result) => {
+//         console.log("Data2 was saved");
+//         console.log("result of promise: ", result);
+//         return savetoDb("Rasika");
+//     })
 
-    .then((result) => {
-        console.log("Data3 was saved");
-        console.log("result of promise: ", result);
-    })
+//     .then((result) => {
+//         console.log("Data3 was saved");
+//         console.log("result of promise: ", result);
+//     })
    
-    .catch( (error) => {
-        console.log("promise was rejected");
-        console.log("result of promise: ",error );
+//     .catch( (error) => {
+//         console.log("promise was rejected");
+//         console.log("result of promise: ",error );
 
-    });
+//     });
 
 
 
