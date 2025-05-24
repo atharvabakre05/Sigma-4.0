c91f1dd187079bc8dbe7791d6f1938a4eb237fa8
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-05-25 00:40:19 +0530

    Started with backend and learnt about exports and require

---
+++ Backend/script.js
@@ -0,0 +1,16 @@
+// let n = 5
+// for (let i = 0; i<=5; i++) {
+//     console.log(i);
+// }
+// console.log("bye!");  
+
+// let args = process.argv;
+// for(let i=0;i<args.length;i++) {
+//     console.log("Hello to ", args[i]);
+// }
+ 
+// const someValue = require("./math");
+// console.log(someValue); 
+
+math = require("./math");
+console.log(math.sum(2,3));
