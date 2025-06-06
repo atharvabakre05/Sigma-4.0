c7ae7df0a90c5eeee922550cd8bc287708835f90
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-06-06 22:52:06 +0530

    Basic HTML page using express

--- Backend/ExpressDir/index.js
+++ Backend/ExpressDir/index.js
@@ -9,4 +9,13 @@ app.listen(port, () => {
 
 app.use((req, res) => {
     console.log("request received");
+    // console.dir(req);
+    // res.send("This is a basic response");
+// res.send({
+//     fruit: 'Apple',
+//     color: 'Red',
+// });
+
+    let code = "<h1>Fruits<ul><li>Orange</li><li>Mango</li></ul></h1>"
+    res.send(code);
 }); 
