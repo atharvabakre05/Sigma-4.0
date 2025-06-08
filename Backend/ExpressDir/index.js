5473d7c9fb4526d6394653584769b0111c71b2f3
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-06-08 20:54:43 +0530

    EJS installation and setup

--- Backend/ExpressDir/index.js
+++ Backend/ExpressDir/index.js
@@ -6,25 +6,27 @@ let port = 8080;
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
 });
-
-
      
 app.get("/", (req, res) => {
     res.send("Hello I am root path");
 });
 
+     
+app.get("/:username/:id",(req, res) => {
+    let {username, id} = req.params;
+    let htmlStr = `<h1> Welcome to this page of @${username} with id:${id}<h1>`
+    res.send(htmlStr); 
+});
+
 app.get("/search", (req, res) => {
-    res.send("You contacted the search path");
+    let {q} = req.query;
+    if(!q) {
+        res.send(`<h1>Nothing searched</h1>   `);
+    }
+    res.send(`<h1>These are the search results for the desired query ${q}</h1>`);
 });
 
-app.get("/login", (req, res) => {
-    res.send("You contacted the login path");
-});
 
-// app.get("*", (req, res) => {
+// app.get(/.*/, (req, res) => {
 //     res.send("This path doesn't exist");
 // });
-
-app.get(/.*/, (req, res) => {
-    res.send("This path doesn't exist");
-});
